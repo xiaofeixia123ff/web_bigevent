b4a7902def96724290b870bccc9d65a232386536
zyf <2095026158@qq.com>
2023-05-25 18:47:23 +0800

    完成了主页的功能开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -5,4 +5,25 @@ $.ajaxPrefilter(function(options){
     // console.log(options.url);
     options.url = 'http://127.0.0.1:3007' + options.url;
     // console.log(options.url);
+
+    // 统一为有权限的接口，设置header请求头
+    if(options.url.indexOf('/my') !== -1){
+        options.headers = {
+            Authorization:localStorage.getItem('token') || ''
+        }
+    }
+
+    // 全局统一挂载 complete 回调函数。
+    options.complete = function(res){
+        console.log('执行了 complete 回调：');
+        // console.log(res);
+        // 在 complete 回到函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据。
+        // console.log(res.responseJSON);
+        if(res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！'){
+            // 1. 强制清空 token
+            localStorage.removeItem('token')
+            // 2. 强制跳转到登录页面
+            location.href = '/login.html'
+        }
+    }
 })
